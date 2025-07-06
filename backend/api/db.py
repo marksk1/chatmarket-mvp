6b036e0f29b6c410a955fcb63269e871f8a90c84
arodrica43 <lexws33@gmail.com>
2025-07-06 02:20:15 +0200

    (1) Adding OAuth2 authentication

---
+++ backend/api/db.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["chat_marketplace"]
+collection = db["user"]  # your MongoDB collection
+
+users_collection = db["users"]
+items_collection = db["items"]
